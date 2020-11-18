d1ab1340a089bf0489eb5cdb5011d454744cbe43
joaopvasconcelos99 <joaopvasconcelos@ua.pt>
2020-11-18 00:26:34 +0000

    second commit

---
+++ app/src/main/java/com/example/icmproject/ClientCabazesFragment.java
@@ -0,0 +1,16 @@
+package com.example.icmproject;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ClientCabazesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.client_cabazes_fragment, container, false);
+    }
+}
--- app/src/main/java/com/example/icmproject/ClientMenuActivity.java
+++ app/src/main/java/com/example/icmproject/ClientMenuActivity.java
@@ -1,50 +1,49 @@
 package com.example.icmproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
-import com.google.android.material.tabs.TabItem;
-import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class ClientMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d("yau","ClientMenuActivity CREATE ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_menu);
-
-        TabLayout tabLayout = findViewById(R.id.tabBar);
-        TabItem tabCabazes = findViewById(R.id.tabCabazes);
-        TabItem tabBookings = findViewById(R.id.tabBookings);
-        ViewPager viewPager = findViewById(R.id.viewPager);
-
-        PagerAdapter pagerAdapter = new
-                PagerAdapter(getSupportFragmentManager(),
-                    tabLayout.getTabCount());
-
-        viewPager.setAdapter(pagerAdapter);
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-
-                Log.d("a", String.valueOf(tab.getPosition()));
-                viewPager.setCurrentItem(tab.getPosition());
-
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ClientCabazesFragment()).commit();
+        }
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_cabazes:
+                            selectedFragment = new ClientCabazesFragment();
+                            break;
+                        case R.id.nav_bookings:
+                            selectedFragment = new ClientBookingsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
